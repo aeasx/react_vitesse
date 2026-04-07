ded3a23dbde81ae01288f08a0641be3564b3feec
肖振洋 <xiaozhenyang@huomakeji.com>
2026-04-07 15:19:22 +0800

    feat: add nav

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,11 @@ function AppContent() {
   const themeContext = useContext(ThemeContext)
   return (
     <div>
-      <nav className="grid grid-cols-[30%_70%] grid-rows-[50px_auto] items-center px-4">
+      <nav
+        className="grid grid-cols-[30%_70%] grid-rows-[50px_auto] items-center px-4 border-b-2 border-gray-300 dark:border-gray-700 mb-1"
+      >
         <ThemeIcon onClick={themeContext.toggleTheme} isDark={themeContext.isDark} />
       </nav>
-      <h1 className="my-10">
-        <ThemeIcon onClick={themeContext.toggleTheme} isDark={themeContext.isDark} />
-      </h1>
-      <h1 className="my-10">
-        <ThemeIcon onClick={themeContext.toggleTheme} isDark={themeContext.isDark} />
-      </h1>
-      <h1 className="my-10">
-        <ThemeIcon onClick={themeContext.toggleTheme} isDark={themeContext.isDark} />
-      </h1>
       <Outlet />
     </div>
   )
